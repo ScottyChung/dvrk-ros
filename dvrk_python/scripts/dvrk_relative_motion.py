5ce73487f07ba390c42d1980141d341d8d1cd310
robotuser <scottc213@gmail.com>
2019-10-24 11:31:39 -0400

    Storing changes before update

--- dvrk_python/scripts/dvrk_relative_motion.py
+++ dvrk_python/scripts/dvrk_relative_motion.py
@@ -19,6 +19,7 @@ class example_application:
         self.t_dvrk_auro = None
         self.data = None
         self.dvrk_home = None
+        self.previous_pose = None
         self.previous_vector = PyKDL.Vector(0, 0, 0)
         # NDI Sensor topic
         self.topic = '/ndi/Tool/position_cartesian_current'
@@ -57,15 +58,17 @@ class example_application:
         return numpy.array([pose.position.x, pose.position.y, pose.position.z])
 
     def get_transformation(self):
+        #Getting Home Position this is offset between the origins of the two coordinate systems
         home = self.data.pose.position
-        set_distance = 0.04
+
+        set_distance = 0.04 #Distance to test each axis
+        #Move in Z,Y,X axis, and normalize vectors to unit vectors
         self.arm.dmove(PyKDL.Vector(0, 0, set_distance))
         rospy.sleep(2)
         z_axis = self.data.pose.position
         z_axis = PyKDL.Vector(z_axis.x - home.x, z_axis.y - home.y, z_axis.z - home.z)
         z_axis.Normalize()
         print('Z_axis: ' + str(z_axis))
-
 
         self.home()
         self.arm.dmove(PyKDL.Vector(0, set_distance, 0))
@@ -84,29 +87,64 @@ class example_application:
         x_axis.Normalize()
         print('X_axis: ' + str(x_axis))
 
-        r_dvrk_auro = PyKDL.Rotation(x_axis, y_axis, z_axis)
-
-        print(r_dvrk_auro)
         self.home()
-        self.t_dvrk_auro = PyKDL.Frame(r_dvrk_auro, PyKDL.Vector(home.x, home.y, home.z))
+
+        #Build rotation matrix
+        r_dvrk_auro = PyKDL.Rotation(x_axis, y_axis, z_axis)
+        print(r_dvrk_auro)
+
+        #Convert home vector into robot frame
+        AB = self.pose_to_vector(self.data.pose)
+        BA = -AB
+        BA_frameB = r_dvrk_auro*PyKDL.Vector(*BA)
+        self.t_dvrk_auro = PyKDL.Frame(r_dvrk_auro, BA_frameB)
         return self.t_dvrk_auro
 
     def run(self):
-        self.home = self.data
+        self.aurora_home = self.data
+        return
         while True:
             self.follow()
 
     def follow(self):
+        #Get pose of sensor
         current_pose = self.pose_to_vector(self.data.pose)
-        #print('Get Pose: {0}'.format(rospy.get_rostime()-start_time))
-        delta = self.pose_to_vector(self.home.pose) - current_pose
-        dvrk_vector = self.t_dvrk_auro.M.Inverse() * PyKDL.Vector(*delta)
-        #print('dvrk_home_position: {0}'.format(self.dvrk_home.p))
-        #print('Transform_vector: {0}'.format(rospy.get_rostime()-start_time))
-        self.arm.dmove(0.2*dvrk_vector, interpolate=True, blocking=False)
-        #print('Move Arm: {0}'.format(rospy.get_rostime()-start_time))
-        self.previous_vector = self.previous_vector + 0.001*dvrk_vector
-        rospy.sleep(0.02)
+        if all(self.previous_vector == current_pose):
+            aurora_delta = np.zeros(3)
+        else:
+
+            #Convert to DVRK frame
+            dvrk_delta = self.t_dvrk_auro.M.Inverse() * PyKDL.Vector(*aurora_delta)
+
+            #Move using delta vector
+            #print(self.dvrk_home.p + dvrk_delta)
+            #self.arm.move(self.dvrk_home.p + dvrk_delta)
+            ''' 
+            #Convert pose to robot frame
+            desired_robot_pose = self.t_dvrk_auro*PyKDL.Vector(*current_pose)
+            z_offset = PyKDL.Vector(0,0,self.dvrk_home.p.z())
+            '''
+            #debug prints
+            '''
+            print('Current sensor position: {0}'.format(current_pose))
+            print('Current arm position: {0}'.format(self.arm.get_current_position()))
+            print('Translation component: {0}'.format(self.t_dvrk_auro.p))
+            print('Z Offset: {0}'.format(z_offset))
+            print(desired_robot_pose + z_offset)
+            '''
+            #self.arm.move(desired_robot_pose + z_offset, blocking=False)
+        
+            """
+            #print('Get Pose: {0}'.format(rospy.get_rostime()-start_time))
+            #delta = self.pose_to_vector(self.home.pose) - current_pose
+            dvrk_vector = self.t_dvrk_auro.M.Inverse() * PyKDL.Vector(*delta)
+            #print('dvrk_home_position: {0}'.format(self.dvrk_home.p))
+            #print('Transform_vector: {0}'.format(rospy.get_rostime()-start_time))
+            self.arm.dmove(0.2*dvrk_vector, interpolate=True, blocking=False)
+            #print('Move Arm: {0}'.format(rospy.get_rostime()-start_time))
+            self.previous_vector = self.previous_vector + 0.001*dvrk_vector
+            """
+            rospy.sleep(0.02)
 
 
 if __name__ == '__main__':
